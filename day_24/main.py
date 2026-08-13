a25e2dd90f62c08952428dc811cd90b6aa0895dc
paz-p <pazpaolo13@gmail.com>
2026-08-13 15:48:33 -0700

    Add files via upload

---
+++ day_24/main.py
@@ -0,0 +1,24 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+names = []
+letters = []
+with open("/Users/paolopaz/PycharmProjects/day_24/Input/Names/invited_names.txt", mode = "r") as name_list:
+    for name in name_list.readlines():
+        names.append(name.strip())
+
+with open("Input/Letters/starting_letter.txt",mode ="r") as example_letter:
+        letter = example_letter.read()
+
+
+
+
+for name in names:
+    new_letter = letter.replace("[name]", name)
+    with open(f"Output/ReadyToSend/letter_for_{name}.txt", mode = "w") as output:
+        output.write(new_letter)
